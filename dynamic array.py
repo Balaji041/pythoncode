f53470890187d10fde6dd01cdffb3a4eb0d88070
balaji <76476394+Balaji041@users.noreply.github.com>
2021-11-30 22:59:36 +0530

    Create dynamic array.py

---
+++ dynamic array.py
@@ -0,0 +1,9 @@
+from array import *
+
+n=int(input("enter array range:"))
+a=array("i",[])
+for i in range(n):
+    ar=int(input("enter a values:"))
+    a.append(ar)
+print(a)
+#dynamicaly taking array numbers...
