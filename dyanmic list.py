6f5324940d6245c6ccdb48f4627e294fcc41f678
balaji <76476394+Balaji041@users.noreply.github.com>
2022-01-27 08:35:10 +0530

    Create dyanmic list.py

---
+++ dyanmic list.py
@@ -0,0 +1,12 @@
+list=[]
+n=int(input("enter a number:"))
+for i in range(n):
+    values=int(input("enter a values:"))
+    list.append(values)
+print(list)
+
+"""enter a number:3
+enter a values:3
+enter a values:2
+enter a values:1
+[3, 2, 1]"""
