d9cf47fb0965f7efb53b1fee7611ca491e1ad33f
balaji <76476394+Balaji041@users.noreply.github.com>
2022-09-22 12:21:58 +0530

    Create fun palindrome.py

---
+++ fun palindrome.py
@@ -0,0 +1,12 @@
+def pan(string):
+    pal=string[::-1]
+    if pal==string:
+        print("palindrome")
+    else:
+        print("not palindrome")
+print(pan=input())
+
+"""
+malayalam
+palindrome
+"""
